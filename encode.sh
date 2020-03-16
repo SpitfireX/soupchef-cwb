6550d04c0531fed53119b916b4a22657d122397d
Timm Weber <timm.weber@me.com>
2020-03-17 00:12:28 +0100

    added lemma as P-attribute to encode commands

--- encode.sh
+++ encode.sh
@@ -4,11 +4,11 @@ echo Creating directory structure
 mkdir -p cwb/data/chefkoch cwb/data/chefkoch_comments cwb/registry/
 
 echo encoding CHEFKOCH recipe corpus
-cwb-encode -d cwb/data/chefkoch/ -R cwb/registry/chefkoch -f output/recipes.vrt.gz -c utf8 -xsB -S s:0+id -S text:0+title+id+url+author+date+yearmonth+year+rating+rating_int+category+category_orig+keywords+related+ingredients -P pos
+cwb-encode -d cwb/data/chefkoch/ -R cwb/registry/chefkoch -f output/recipes.vrt.gz -c utf8 -xsB -S s:0+id -S text:0+title+id+url+author+date+yearmonth+year+rating+rating_int+category+category_orig+keywords+related+ingredients -P pos -P lemma
 echo making CHEFKOCH recipe corpus
 cwb-make -r cwb/registry/ -M 2000 -V CHEFKOCH
 
 echo encoding CHEFKOCH_COMMENTS corpus
-cwb-encode -d cwb/data/chefkoch_comments/ -R cwb/registry/chefkoch_comments -f output/comments.vrt.gz -c utf8 -xsB -S s:0+id -S text:0+id+parent+author+date+yearmonth+year+datetime_orig -P pos
+cwb-encode -d cwb/data/chefkoch_comments/ -R cwb/registry/chefkoch_comments -f output/comments.vrt.gz -c utf8 -xsB -S s:0+id -S text:0+id+parent+author+date+yearmonth+year+datetime_orig -P pos -P lemma
 echo making CHEFKOCH_COMMENTS corpus
 cwb-make -r cwb/registry/ -M 2000 -V CHEFKOCH_COMMENTS
